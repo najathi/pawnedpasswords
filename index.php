154a6dc8267b778f772feaef7841e989f83af7f2
najathi <najathi777@gmail.com>
2021-02-22 11:17:14 +0530

    initsia;

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+function checkPawnedPasswords(string $password) : int
+{
+    $sha1 = strtoupper(sha1($password));
+    $data = file_get_contents('https://api.pwnedpasswords.com/range/'.substr($sha1, 0, 5));
+    echo $sha1;
+    if (FALSE !== strpos($data, substr($sha1, 5))) {
+        $data = explode(substr($sha1, 5).':', $data);
+        $count = (int) $data[1];
+    }
+    return $count ?? 0;
+}
+
+echo checkPawnedPasswords("abcd1234");
