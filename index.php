a80b03f560ca5018416dacf137b7b02fcce92a12
najathi <najathi777@gmail.com>
2021-02-22 11:45:52 +0530

    update code

--- index.php
+++ index.php
@@ -4,12 +4,15 @@ function checkPawnedPasswords(string $password) : int
 {
     $sha1 = strtoupper(sha1($password));
     $data = file_get_contents('https://api.pwnedpasswords.com/range/'.substr($sha1, 0, 5));
-    echo $sha1;
+
     if (FALSE !== strpos($data, substr($sha1, 5))) {
         $data = explode(substr($sha1, 5).':', $data);
         $count = (int) $data[1];
     }
+
     return $count ?? 0;
 }
+
+echo "<br/><br/>";
 
 echo checkPawnedPasswords("abcd1234");
